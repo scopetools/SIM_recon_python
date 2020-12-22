8c4fdfa4b0d3077b747e612ac527d43eee37cb60
Matt McCormick <matt.mccormick@kitware.com>
2020-12-22 11:55:15 -0500

    Testing data check

--- tests.py
+++ tests.py
@@ -1,3 +1,15 @@
+from pathlib import Path
+import os
+import sys
+
+script_dir = Path(__file__).resolve().parent
+data_dir = (script_dir / 'data').resolve()
+print(data_dir)
+if not (data_dir / 'Cell_3phase.tif').exists():
+    print('Testing data not available.')
+    sys.exit(1)
+
+
 def test_package():
     import simtk
     assert hasattr(simtk, '__version__')
