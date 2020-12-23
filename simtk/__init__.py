bd2491ff3e89c35de3dfe282d718ceef6e7d5f86
Matt McCormick <matt.mccormick@kitware.com>
2020-12-23 08:14:43 -0500

    Initial load_image

--- simtk/__init__.py
+++ simtk/__init__.py
@@ -1,3 +1,60 @@
 """sim: structured illumination microscopy"""
 
 __version__ = "0.1.0"
+
+__all__ = [
+        'load_image',
+        ]
+
+import itk
+import numpy as np
+
+def load_image(filename, nphases=3, norientations=1, spacing=None):
+    """Load SIM acquisition data from an image file.
+
+    Read the filename containing multiple information components, reformat the
+    data into a multi-component vector image, and optionally override the voxel
+    spacing information.
+
+    Parameters
+    ----------
+
+    filename: str
+        Path to the SIM acquisition data, often a TIFF file.
+    nphases: int
+        Number of phases acquired in every plane.
+    norientations: int
+        Number of orientations acquired in every plane.
+    spacing: array of float's
+        Override image spacing metadata with the provided [dx, dy, dz] spacing.
+
+    Returns
+    -------
+
+    itk.VectorImage
+        3D, multi-component SIM acquition image. Each component corresponds to an
+        specific phase and orientation information component.
+    """
+
+    image = itk.imread(filename)
+    image.DisconnectPipeline()
+    data_view = itk.array_view_from_image(image)
+
+    size = itk.size(image)
+    components = nphases*norientations
+
+    result_shape = list(data_view.shape)
+    result_shape[0] = int(result_shape[0] / components)
+    result_shape = result_shape + [components,]
+    result = np.empty(result_shape, dtype=np.float32)
+    for component in range(components):
+        result[:,:,:,component] = data_view[component::components,:,:]
+
+    result = itk.PyBuffer[itk.VectorImage[itk.F, 3]].GetImageViewFromArray(result, is_vector=True)
+    result.SetOrigin(image.GetOrigin())
+    result.SetSpacing(image.GetSpacing())
+    result.SetDirection(image.GetDirection())
+    if spacing is not None:
+        result.SetSpacing(spacing)
+
+    return result
--- tests.py
+++ tests.py
@@ -4,12 +4,17 @@ import sys
 
 script_dir = Path(__file__).resolve().parent
 data_dir = (script_dir / 'data').resolve()
-print(data_dir)
 if not (data_dir / 'Cell_3phase.tif').exists():
     print('Testing data not available.')
     sys.exit(1)
 
+test_psf_file = str(data_dir / 'PSF_3phase.tif')
+
+import simtk
 
 def test_package():
     import simtk
     assert hasattr(simtk, '__version__')
+
+def test_load_image():
+    image = simtk.load_image(test_psf_file, spacing=[0.11, 0.11, 0.1])
